9e515e0320139530b4fd0da274242eeafc2a6ade
Hong Ray <119933763+HongRay@users.noreply.github.com>
2023-07-23 18:00:31 +0800

    Include authentication testing

--- locust_testing/index.js
+++ locust_testing/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+//Basic load testing request 
 app.get("/hello", (req, res) => {
   res.send("Hello");
 });
@@ -12,6 +13,31 @@ app.get("/slow", (req, res) => {
   }, 2000);
 });
 
+//Authentication request 
+//Simple authentication procedure where server sends client the token 
+app.get("/login", (req, res) => {
+  res.json({
+    //secret token is sent back to the client for authentication
+    token: "secret-token",
+  });
+});
+
+//This func does the authentication and checks for correct token
+const loginMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token !== "secret-token") {
+    res.status(403).send("Not logged in");
+  } else {
+    next();
+  }
+};
+
+//To access /secret page, include headers; 'Authorization': 'secret-token' 
+app.get("/secret", loginMiddleware, (req, res) => {
+  res.send("Welcome to the secret page");
+});
+
+//Listening to port 3000
 app.listen(PORT, () => {
   console.log("Server is up");
 });
--- locust_testing/locust.py
+++ locust_testing/locust.py
@@ -7,6 +7,7 @@ from locust import HttpUser, task, between
 
 # HttpUser is the user agent that can visit different cdend points
 class WebsiteUser(HttpUser):
+    '''Basic load testing '''
     # This is how long (s) it takes after it visited an endpoint  
     wait_time = between(1, 5)
 
@@ -26,3 +27,26 @@ class WebsiteUser(HttpUser):
     def post_req(self):
         self.client.post("/login", json={"username":"foo", "password":"bar"})
     '''
+
+    '''Authentication testing '''
+    # Boilerplate for storing token in a variable 
+    def __init__(self, parent):
+        super(WebsiteUser, self).__init__(parent)
+        self.token = ""
+
+    wait_time = between(1, 2)
+    
+    # Login to get token value 
+    def on_start(self):
+        with self.client.get(url="/login") as response:
+            self.token = response.json()["token"]
+
+    # Access /secret with token obtain in on_start method 
+    @task
+    def secret_page(self):
+        self.client.get(url="/secret", headers={"authorization": self.token})
+    
+    # test for failed login
+    @task
+    def failed_login(self):
+        self.client.get(url="/secret", headers={"authorization": "wrong-token"})
