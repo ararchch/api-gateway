712957091f06317b48648166ba24bada62976592
Hong Ray <119933763+HongRay@users.noreply.github.com>
2023-07-27 18:56:36 +0800

    Script testing

--- Script-testing/main.go
+++ Script-testing/main.go
@@ -5,39 +5,34 @@ import (
   "strings"
   "net/http"
   "io/ioutil"
-  //"github.com/ararchch/api-gateway/Script-testing"
 )
 
 func main() {
 
   url := "http://127.0.0.1:8080/multiply"
   method := "POST"
-  fmt.Println("here1")
   payload := strings.NewReader(`{` + "" +` "FirstNum": "6",`+ "" +` "SecondNum": "3"`+""+`}`)
-  fmt.Println("here2")
   client := &http.Client {
   }
   req, err := http.NewRequest(method, url, payload)
 
   if err != nil {
-    fmt.Println("errres 1")
+    fmt.Println(err)
     return
   }
   req.Header.Add("Content-Type", "application/json")
 
   res, err := client.Do(req)
   if err != nil {
-    fmt.Println("err res")
+    fmt.Println(err)
     return
   }
   defer res.Body.Close()
 
   body, err := ioutil.ReadAll(res.Body)
   if err != nil {
-    fmt.Println("Error body")
+    fmt.Println(err)
     return
   }
-  fmt.Println("here")
   fmt.Println(string(body))
-  fmt.Println(res.StatusCode)
 }
