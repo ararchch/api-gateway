76264c15ca4279f1bb409d5629c49555f9dd1cdd
Archit <architg.sch@gmail.com>
2023-08-01 00:51:39 +0800

    remove comments from ping.go

--- hertz-http-server/biz/handler/ping.go
+++ hertz-http-server/biz/handler/ping.go
@@ -22,40 +22,20 @@ func Ping(ctx context.Context, c *app.RequestContext) {
 	service := c.Param("service")
 	method := c.Param("method")
 
-	//fmt.Println(string(c.Request.Body()))
-
 	kitexClient, err := gatewayUtils.GenerateClient(service)
 	if (err != nil) {
-		fmt.Println("could not locate service\n")
+		fmt.Println("could not locate service")
 		panic(err)
 	}
 
-	// type AdditionRequest struct {
-	// 	FirstNum  string 
-	// 	SecondNum string 
-	// }
-	// var req AdditionRequest
-
-	// type AdditionResponse struct {
-	// 	Sum string 
-	// }
-	//var resp AdditionResponse
-
-	// bind error params to req (pre-generated)
-	// err = c.BindAndValidate(&req)
-	// if err != nil {
-	// 	c.String(consts.StatusBadRequest, err.Error())
-	// 	return
-	// }
-
 	resp, err := gatewayUtils.MakeRpcRequest2(ctx, kitexClient, method, string(c.Request.Body()))
 	if (err != nil){
-		fmt.Println("RPC request error \n")
+		fmt.Println("RPC request error")
 		panic(err)
 	}
 
 	var obj interface{}
 	json.Unmarshal([]byte(resp.(string)), &obj)
-	
+
 	c.JSON(consts.StatusOK, obj)
 }
